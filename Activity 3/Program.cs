dc7c2c2ba87edf76d4e29281f74841ef0a588ab1
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2024-02-06 22:19:35 +0800

    Add files via upload

---
+++ Activity 3/Program.cs
@@ -0,0 +1,15 @@
+﻿double price = 2000 ;
+double discountRate = 0.10;
+
+
+Console.WriteLine("Enter value for price: 2000");
+if (price > discountRate)
+{
+    double discount = price * discountRate;
+    Console.WriteLine("Discount is: " + discount);
+
+}
+else 
+{
+    Console.WriteLine("No discount");
+} 
