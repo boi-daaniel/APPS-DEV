447ac9509d006a11facf2f50c50f5df6810ac2d2
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2024-02-06 22:20:51 +0800

    Update Program.cs

--- Activity 3/Program.cs
+++ Activity 3/Program.cs
@@ -1,13 +1,11 @@
 ﻿double price = 2000 ;
 double discountRate = 0.10;
 
-
 Console.WriteLine("Enter value for price: 2000");
 if (price > discountRate)
 {
     double discount = price * discountRate;
     Console.WriteLine("Discount is: " + discount);
-
 }
 else 
 {
