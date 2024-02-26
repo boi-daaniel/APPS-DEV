92f661308ff62b30bf4889ecc46926a549c86dce
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2024-02-27 00:07:29 +0800

    Update Form1.cs

--- Activity 5/Form1.cs
+++ Activity 5/Form1.cs
@@ -1,17 +1,43 @@
-﻿int TotalBags = 52;
-double bagPrice = 5.50;
-double largePrice = 3.00;
-double mediumPrice = 2.50;
-double smallPrice = 2.00;
-int[] amount = {2, 1, 1};
+using System.Xml.Linq;
 
-double result = TotalBags * bagPrice;
-double totalResult = result + (largePrice * amount[0]) + (mediumPrice * amount[1]) + (smallPrice * amount[2]);
+namespace CustomerDataEntry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Gender, Hobby, Status = "";
+
+            if (rdbMale.Checked) 
+                Gender = "Male";
+            else 
+                Gender = "Female";
+
+            if (chk1.Checked && chk2.Checked)
+                Hobby = "Reading and Painting";
+            else if (chk1.Checked)
+                Hobby = "Reading";
+            else if (chk2.Checked)
+                Hobby = "Painting";
+            else
+                Hobby = "No Hobby Selected";
 
 
-Console.WriteLine("Number of Bags Ordered: " + TotalBags);
-Console.WriteLine("Boxes Used: ");
-Console.WriteLine("Large - " + amount[0]);
-Console.WriteLine("Medium - " + amount[1]);
-Console.WriteLine("Small - " + amount[2]);
-Console.WriteLine("Your total cost is: $" + totalResult);
+            if (rdbMarried.Checked) 
+                Status = "Married";
+            else 
+                Status = "Single";
+            
+            Preview customerData = new Preview();
+
+            customerData.SetValues(txt1.Text, cmbCountry.Text, Gender, Hobby, Status);
+            customerData.Show();
+
+        }
+    }
+}
