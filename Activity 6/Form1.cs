b4ae84e4cd35c2b15552a80270557345f8e0e2b4
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2024-02-27 18:41:57 +0800

    Update Form1.cs

--- Activity 6/Form1.cs
+++ Activity 6/Form1.cs
@@ -1,17 +1,54 @@
-﻿int TotalBags = 52;
-double bagPrice = 5.50;
-double largePrice = 3.00;
-double mediumPrice = 2.50;
-double smallPrice = 2.00;
-int[] amount = {2, 1, 1};
+namespace Sales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-double result = TotalBags * bagPrice;
-double totalResult = result + (largePrice * amount[0]) + (mediumPrice * amount[1]) + (smallPrice * amount[2]);
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
+        }
 
-Console.WriteLine("Number of Bags Ordered: " + TotalBags);
-Console.WriteLine("Boxes Used: ");
-Console.WriteLine("Large - " + amount[0]);
-Console.WriteLine("Medium - " + amount[1]);
-Console.WriteLine("Small - " + amount[2]);
-Console.WriteLine("Your total cost is: $" + totalResult);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double text1 = Convert.ToDouble(txt1.Text);
+            double text2 = Convert.ToDouble(txt2.Text);
+            double text3 = Convert.ToDouble(txt3.Text);
+
+            double TotalSale = text1 + text2 + text3;
+            txt4.Text = TotalSale.ToString();
+
+            double commission = 0;
+            String percent = "";
+
+            if (chk1.Checked)
+            {
+                commission = TotalSale * 0.5;
+                percent = "50%";
+            }
+            else if (chk2.Checked)
+            {
+                commission = TotalSale * 0.1;
+                percent = "10%";
+            }
+            else if (chk3.Checked)
+            {
+                commission = TotalSale * 0.15;
+                percent = "15%";
+            }
+            else
+            {
+                percent = "No Percentage Selected";
+            }
+
+            double totalAmount = TotalSale + commission;
+
+            Preview result = new Preview();
+            result.setValues(commission, percent, totalAmount);
+            result.Show();
+        }
+    }
+}
