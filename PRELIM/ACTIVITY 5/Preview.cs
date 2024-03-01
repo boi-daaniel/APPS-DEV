c06054c43f9d5a69a9a2c4a16e45a2c73a6702c3
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2024-03-02 02:36:32 +0800

    Update Preview.cs

--- PRELIM/ACTIVITY 5/Preview.cs
+++ PRELIM/ACTIVITY 5/Preview.cs
@@ -1,17 +1,19 @@
-﻿int TotalBags = 52;
-double bagPrice = 5.50;
-double largePrice = 3.00;
-double mediumPrice = 2.50;
-double smallPrice = 2.00;
-int[] amount = {2, 1, 1};
+namespace CustomerDataEntry
+{
+    public partial class Preview : Form
+    {
+        public Preview()
+        {
+            InitializeComponent();
+        }
 
-double result = TotalBags * bagPrice;
-double totalResult = result + (largePrice * amount[0]) + (mediumPrice * amount[1]) + (smallPrice * amount[2]);
-
-
-Console.WriteLine("Number of Bags Ordered: " + TotalBags);
-Console.WriteLine("Boxes Used: ");
-Console.WriteLine("Large - " + amount[0]);
-Console.WriteLine("Medium - " + amount[1]);
-Console.WriteLine("Small - " + amount[2]);
-Console.WriteLine("Your total cost is: $" + totalResult);
+        public void SetValues(string Name, string Country, string Gender, string Hobby, string Status)
+        {
+            lblName.Text = Name;
+            lblCountry.Text = Country;
+            lblGender.Text = Gender;
+            lblHobby.Text = Hobby;
+            lblStatus.Text = Status;
+        }
+    }
+}
