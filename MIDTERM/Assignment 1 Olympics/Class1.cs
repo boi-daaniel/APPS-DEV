72bc84e60c0054e7233798258321b1fb827f59ff
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2024-03-13 00:11:12 +0800

    Add files via upload

---
+++ MIDTERM/Assignment 1 Olympics/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceTest
+{
+    internal class Class1
+    {
+
+        public void inputScore()
+        {
+            double totalScores = 0;
+            double[] contestantsScore = new double[8];
+
+            for (int i = 0; i < contestantsScore.Length; i++)
+            {
+                Console.Write($"Enter the score of judge [{i + 1}] :");
+                contestantsScore[i] = Convert.ToDouble(Console.ReadLine());
+                totalScores += contestantsScore[i];
+            }
+
+            totalScores -= contestantsScore.Max();
+            totalScores -= contestantsScore.Min();
+            Console.WriteLine($"The contestant receives a total of {totalScores.ToString("N2")} points.");
+        }
+
+    }
+}
---
+++ MIDTERM/Assignment 1 Olympics/Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceTest
+{
+    internal class Test
+    {
+        public static void Main(String[] strings)
+        {
+            Class1 olympics = new Class1();
+            olympics.inputScore();  
+        }
+    }
+}
