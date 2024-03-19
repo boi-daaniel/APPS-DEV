70cb0432e7389e87a5dd09035c64954d1da2357e
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2024-03-19 19:23:19 +0800

    Add files via upload

---
+++ MIDTERM/Lab Quiz Array/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3score
+{
+    internal class Class1
+    {
+        public void inputScore()
+        {
+            string[] name = new string[3];
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                double scores,average;
+                double sum = 0;
+                Console.Write($"Enter name[{i+1}]: ");
+                name[i] = Console.ReadLine();
+                Console.WriteLine("Enter scores: ");
+                for (int j = 0; j < name.Length; j++)
+                {
+                    scores = Convert.ToDouble(Console.ReadLine());
+                    sum += scores;
+                }
+                Console.WriteLine($"The sum is: {sum.ToString("N1")}");
+                average = sum / name.Length;
+                Console.WriteLine($"The average is: {average.ToString("N1")}");
+            }
+        }
+    }
+}
---
+++ MIDTERM/Lab Quiz Array/ClasstTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3score
+{
+    internal class ClasstTest
+    {
+        public static void Main(String[] args)
+        {
+            Class1 run = new Class1();
+            run.inputScore();
+        }
+    }
+}
